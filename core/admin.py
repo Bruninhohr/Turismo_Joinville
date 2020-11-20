848eeeb9428a5a68df52ae45e2911bec18e3821d
Bruno Henrique Rodrigues <56198586+Bruninhohr@users.noreply.github.com>
2020-11-20 20:45:06 -0300

    Add files via upload

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import Comentario
+from core.models import Eventos
+# Register your models here.
+
+admin.site.register(Comentario)
+admin.site.register(Eventos)
